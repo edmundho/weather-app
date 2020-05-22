cc6196e6c3961e19d8779222faf5da1354374120
Ed <edmundho9@gmail.com>
2020-05-21 18:08:11 -0700

    cache weather data for 30 minutes

--- app/controllers/api/weather_controller.rb
+++ app/controllers/api/weather_controller.rb
@@ -7,9 +7,18 @@ module Api
       zip_code = /\d{5}(?!.*\d)/.match(params[:street])
 
       zip_data = get_zip_data(zip_code)
-      weather_info = get_aggregate_info(zip_data[:lat], zip_data[:lon]).merge({
-        location: zip_data
-      })
+
+      cached_data = Rails.cache.read(zip_code)
+
+      if cached_data
+        weather_info = cached_data.merge({ cached: true })
+      else
+        weather_info = get_aggregate_info(zip_data[:lat], zip_data[:lon]).merge({
+          location: zip_data,
+          cached: false
+        })
+        Rails.cache.write(zip_code, weather_info, expires_in: 30.minutes)
+      end
 
       render json: weather_info
     end
