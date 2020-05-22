e688fe888c33e24edb0c10a11f1f94648b72c7a5
Ed <edmundho9@gmail.com>
2020-05-21 17:32:45 -0700

    extract api code into helper module

--- app/controllers/api/weather_controller.rb
+++ app/controllers/api/weather_controller.rb
@@ -1,42 +1,17 @@
 module Api
   class WeatherController < ApplicationController
     skip_before_action :verify_authenticity_token
+    include ApiUtils
 
     def get_weather_info
       zip_code = /\d{5}(?!.*\d)/.match(params[:street])
 
-      address_data = location_data(zip_code)
-      weather_info = get_aggregate_info(address_data[:lat], address_data[:lon]).merge({
-        location: address_data
+      zip_data = get_zip_data(zip_code)
+      weather_info = get_aggregate_info(zip_data[:lat], zip_data[:lon]).merge({
+        location: zip_data
       })
 
       render json: weather_info
-    end
-
-    def location_data(zip)
-      url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=#{zip}"
-      body = RestClient.get(url)
-      data = JSON.parse(body)['records'][0]['fields']
-      coords = {
-        lat: data['geopoint'][0],
-        lon: data['geopoint'][1],
-        zip: data['zip'],
-        city: data['city'],
-        state: data['state']
-      }
-    end
-
-    def get_aggregate_info(lat, lon)
-      api_key = ENV['OWM_KEY']
-      url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=hourly,minutely&appid=#{api_key}"
-      body = RestClient.get(url)
-      data = JSON.parse(body)
-      current_weather = data['current']
-      forecast = data['daily']
-      {
-        current_weather: extract_current_weather(current_weather),
-        forecast: extract_forecast_data(forecast)
-      }
     end
   end
 end
---
+++ app/controllers/concerns/api_utils.rb
@@ -0,0 +1,50 @@
+module ApiUtils
+  def get_zip_data(zip_code)
+    url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=#{zip_code}"
+    response = RestClient.get(url)
+    data = JSON.parse(response)['records'][0]['fields']
+    {
+      lat: data['geopoint'][0],
+      lon: data['geopoint'][1],
+      zip: data['zip'],
+      city: data['city'],
+      state: data['state']
+    }
+  end
+
+  def get_aggregate_info(lat, lon)
+    api_key = ENV['OWM_KEY']
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=hourly,minutely&appid=#{api_key}"
+    body = RestClient.get(url)
+    data = JSON.parse(body)
+    current_weather = data['current']
+    forecast = data['daily']
+    {
+      current_weather: extract_current_weather(current_weather),
+      forecast: extract_forecast_data(forecast)
+    }
+  end
+
+  def extract_current_weather(data)
+    {
+      current_temp: data['temp'],
+      feels_like: data['feels_like'],
+      condition: data['weather'][0]['main'],
+      date: Time.at(data['dt']).to_date
+    }
+  end
+
+  def extract_forecast_data(data)
+    forecast = []
+    data.each do |day|
+      date = Time.at(day['dt']).to_date
+      forecast << {
+        date: date,
+        high: day['temp']['max'],
+        low: day['temp']['min'],
+        condition: day['weather'][0]['main']
+      }
+    end
+    forecast
+  end
+end
