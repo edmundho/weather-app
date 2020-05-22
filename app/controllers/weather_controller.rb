0c851b4b8806108599af7869fb967718d28ae85d
Ed <edmundho9@gmail.com>
2020-05-22 09:59:16 -0700

    add error handling

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,6 +4,9 @@ class WeatherController < ApplicationController
 
   def get_weather_info
     zip_code = /\d{5}(?!.*\d)/.match(params[:street])
+    if !zip_code
+      render json: 'Invalid zip code entered', status: 422 and return
+    end
 
     zip_data = get_zip_data(zip_code)
 
