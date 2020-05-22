4c22d61c02b5265536706a817537d5fb98111d7e
Ed <edmundho9@gmail.com>
2020-05-21 17:17:46 -0700

    add weather api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    match :weather, to: 'weather#get_weather_info', via: [:get, :post]
+  end
 end
